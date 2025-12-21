fc7c9ea1373b045d37e6ce77d8fbbe4fefbd4fa6
Bob Gosse <shotlogic.studio@gmail.com>
2025-12-21 11:05:26 -0500

    fix: remove extra closing div tags

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -459,8 +459,6 @@ onClick={() => setExpandedScene(expandedScene === scene.number ? null : scene.nu
                     </div>
                   )}
                   
-                </div>
-                    </div>
                   </div>
                 </div>
               ))}
