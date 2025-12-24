135b7320a86c0c1719ba2345c215065b604f06a2
Bob Gosse <shotlogic.studio@gmail.com>
2025-12-24 10:52:00 -0500

    Add claim-orphans route to server

--- server.mjs
+++ server.mjs
@@ -80,6 +80,11 @@ app.post('/api/projects/save', async (req, res) => {
 });
 
 app.delete('/api/projects/delete', async (req, res) => {
+
+// Claim orphan projects
+app.post("/api/projects/claim-orphans", async (req, res) => {
+  await apiHandler(req, res, join(__dirname, "api/projects/claim-orphans.ts"));
+});
   await apiHandler(req, res, join(__dirname, 'api/projects/delete.ts'));
 });
 // Save scene edits
